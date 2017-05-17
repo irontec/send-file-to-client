aa7fba39e67acd43694742d69f69a863d84b132d
ddniel16 <ddniel16@gmail.com>
2017-05-17 12:51:39 +0200

    SendFileToClient.php

--- src/SendFileToClient.php
+++ src/SendFileToClient.php
@@ -1,26 +1,40 @@
 <?php
-
 /**
  * En este archivo se declara la clase "SendFileToClient".
- * @link    https://github.com/ddniel16
- * @author  ddniel16 <dani@irontec.com>
- * @license https://opensource.org/licenses/EUPL-1.1 European Union Public Licence (V. 1.1)
- * @package SendFileToClient\SendFileToClient
+ *
+ * PHP Version 5+
+ *
+ * @category Transport
+ * @package  SendFileToClient
+ * @author   ddniel16 <daniel@irontec.com>
+ * @license  https://opensource.org/licenses/EUPL-1.1 European Union Public Licence (V. 1.1)
+ * @link     https://github.com/irontec
  */
+
 namespace SendFileToClient;
 
 /**
  * Esta clase en encagar de enviar un archivo grande en partes con el estado:
  * HTTP/1.1 206 Partial Content
- * @version 0.0.3
+ *
+ * @category Transport
+ * @package  SendFileToClient
+ * @author   ddniel16 <daniel@irontec.com>
+ * @license  https://opensource.org/licenses/EUPL-1.1 European Union Public Licence (V. 1.1)
+ * @link     https://github.com/irontec
  */
+
 class SendFileToClient
 {
 
     /**
-     * @param string $path
-     * @param string $fileName
-     * @param array $options
+     * Procesa el archivo para el envio en partes.
+     *
+     * @param string $path     ruta al archivo
+     * @param string $fileName nombre del archivo
+     * @param array  $options  headers adicionales
+     *
+     * @return Partial file
      */
     public function sendFile($path, $fileName, array $options = array())
     {
@@ -39,14 +53,7 @@ class SendFileToClient
         if ($httpRange) {
 
             $matches = array();
-            if (
-                preg_match(
-                    '/bytes=\h*(\d+)-(\d*)[\D.*]?/i',
-                    $httpRange,
-                    $matches
-                )
-            ) {
-
+            if (preg_match('/bytes=\h*(\d+)-(\d*)[\D.*]?/i', $httpRange, $matches)) {
                 $begin  = intval($matches[1]);
                 if (!empty($matches[2])) {
                     $end = intval($matches[2]);
@@ -98,8 +105,13 @@ class SendFileToClient
     }
 
     /**
-     * @param string $path
+     * Comprueba que el archivo existe
+     *
+     * @param string $path ruta del archivo
+     *
      * @throws \Exception
+     *
+     * @return NULL
      */
     protected function _checkFile($path)
     {
@@ -124,8 +136,11 @@ class SendFileToClient
     }
 
     /**
-     * @param string $path
-     * @return string
+     * Obtiene el MimeType del archivo
+     *
+     * @param string $path rutal del archivo
+     *
+     * @return string MimeType del archivo
      */
     protected function _fileInfo($path)
     {
